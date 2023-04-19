8d96b3546fcd714a72327b67b03e2cc0485bd664
Andre Luiz <andre.java.ads.2022@gmal.com>
2023-04-19 16:23:01 -0300

    Enviando Program.cs

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ExercicioWhile02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            int y = 1;
+           
+           while (x != 0 && y != 0)
+            {
+                string [] vet = Console.ReadLine().Split(' ');
+                
+                x = int.Parse(vet[0]);
+                y = int.Parse(vet[1]);
+
+                if(x > 0 && y > 0)
+                {
+                    Console.WriteLine("primeiro");
+                }
+                else if(x > 0 && y < 0)
+                {
+                    Console.WriteLine("quarto");
+                }
+                else if(x < 0 && y < 0)
+                {
+                    Console.WriteLine("terceiro");
+                }
+                else if(x < 0 && y > 0)
+                {
+                    Console.WriteLine("segundo");
+                }
+            }
+        
+
+            
+           
+        }
+    }
+}
